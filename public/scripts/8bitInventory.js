25617adb614458851f8537c6111bbddb8bbb1a37
John Amakye <john.amakye@minneapolismn.gov>
2017-01-06 15:34:26 -0600

    data persistence added

--- public/scripts/8bitInventory.js
+++ public/scripts/8bitInventory.js
@@ -3,12 +3,17 @@ var sizes = ['small', 'medium', 'large'];
 var colors = ['red', 'orange', 'yellow', 'green', 'mermaid treasure', 'blue', 'purple'];
 
 ////// global array of items in inventory //////
-var items = [];
+  var items = [];
 
 $(document).ready(function() {
 
     $("#addItem").on("click", function() {
         addObject();
+        $('#outputDiv').html('');
+    });
+
+    $("#searchItem").on("click",function() {
+        getObjects();
     });
 
     var addObject = function() {
@@ -45,21 +50,23 @@ $(document).ready(function() {
         ////// TODO: replace the stuff in this function with getting items from the database ////////
         ////// hint: make a get call to the getInventory and use it's response data to fill the items array ////////\
         // test get function
+
+        var searchColor = $(".search-color").val();
+        var searchSize = $(".search-size").val();
         $.ajax({
             type: 'GET',
             url: '/getItems',
             success: function(response) {
                 console.log('back from get call:', response);
-                displayMatches( response );
+                searchInventory( response , searchColor, searchSize );
             }, // end success
             error: function() {
                     console.log('error with ajax call...');
                 } // end error
         }); // end ajax
-        items.push(response);
     }; // end getInventory
 
-    var searchInventory = function(colorCheck, sizeCheck) {
+    var searchInventory = function(items, colorCheck, sizeCheck) {
         console.log('in searchInventory. Looking for:', colorCheck, sizeCheck);
         // array of matches
         var matches = [];
@@ -70,7 +77,7 @@ $(document).ready(function() {
             } // end if
         } // end for
         console.log('matches:', matches);
-        ////// TODO: display matches
+        //// TODO: display matches
         displayMatches(matches);
     }; // end searchInventory
 
@@ -78,15 +85,16 @@ $(document).ready(function() {
         // empty outputDiv
         $('#outputDiv').html('');
         var outputText = '';
+        console.log("allmatches is:", allMatches);
         for (var i = 0; i < allMatches.length; i++) {
-            $('#outputDiv').append('<p>' + allMatches[i].name + '</p>');
+            $('#outputDiv').append('<p> Found: ' + allMatches[i].item + " " + allMatches[i].color + " " + allMatches[i].size + '</p>');
         } // end for
     }; // end displayMatches
 
-    // get objects when doc is ready
-    getObjects();
-    // the below are tests to show what is returned when running searchInventory
-    addObject('blue', 'blueberry', 'small');
-    searchInventory('blue', 'small');
-    searchInventory('blue', 'large');
+    // // get objects when doc is ready
+    // getObjects();
+    // // the below are tests to show what is returned when running searchInventory
+    // addObject('blue', 'blueberry', 'small');
+    // searchInventory('blue', 'small');
+    // searchInventory('blue', 'large');
 }); // end doc ready
